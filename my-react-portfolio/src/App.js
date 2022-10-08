58301047c6284412488cf942d19e8e30c4ebfeaa
Maha <maha.spearmint@gmail.com>
2022-10-08 16:20:56 +0800

    Basic files added

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -1,23 +1,31 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import logo from './logo.svg';
-import './App.css';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
 
 function App() {
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+    {
+      path: '/contact',
+      element: <Contact/>
+      
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+
+
+      <RouterProvider router={router} />
+
+
     </div>
   );
 }
